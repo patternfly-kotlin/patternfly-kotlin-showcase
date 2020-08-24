e13a418805de40e73561f14c86d40866f716c784
Harald Pehl <harald.pehl@gmail.com>
2020-08-24 11:34:58 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    id("org.jetbrains.kotlin.js") version "1.4.0"
+}
+
+group = "org.patternfly"
+version = "0.1-SNAPSHOT"
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+    maven("https://oss.jfrog.org/artifactory/jfrog-dependencies")
+    jcenter()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-js"))
+    implementation( "org.jetbrains:kotlin-extensions:1.0.1-pre.112-kotlin-1.4.0")
+    implementation("dev.fritz2:core:0.8-SNAPSHOT")
+    implementation("org.patternfly:patternfly-fritz2:0.1-SNAPSHOT")
+    implementation(npm("@patternfly/patternfly", "4.23.3"))
+    implementation(npm("clipboard", "2.0.6"))
+    implementation(npm("highlight.js", "10.1.1"))
+    implementation(devNpm("file-loader", "6.0.0"))
+}
+
+kotlin {
+    js {
+        browser {
+            webpackTask {
+                cssSupport.enabled = true
+            }
+            runTask {
+                cssSupport.enabled = true
+            }
+        }
+        binaries.executable()
+    }
+}
---
+++ src/main/kotlin/org/patternfly/showcase/Main.kt
@@ -0,0 +1,33 @@
+package org.patternfly.showcase
+
+import dev.fritz2.routing.Router
+import dev.fritz2.routing.StringRoute
+import kotlinext.js.require
+import kotlinx.browser.document
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import kotlinx.dom.clear
+
+fun main() {
+    require("@patternfly/patternfly/patternfly.css")
+    require("@patternfly/patternfly/patternfly-addons.css")
+    require("clipboard/dist/clipboard")
+    require("highlight.js/lib/core")
+    require("highlight.js/lib/languages/kotlin")
+    require("highlight.js/styles/github.css")
+
+    val router = Router(StringRoute(Places.HOME))
+    document.body?.let { body ->
+        Skeleton(router).forEach { body.append(it.domNode) }
+    }
+
+    MainScope().launch {
+        router.collect { place ->
+            document.querySelector("#main")?.let { main ->
+                main.clear()
+                Places.lookup(place).forEach { main.append(it.domNode) }
+            }
+        }
+    }
+}
