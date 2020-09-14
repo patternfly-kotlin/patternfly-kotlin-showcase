e231a1f733ffbd8eae5ccd8eca854285173528b7
Harald Pehl <harald.pehl@gmail.com>
2020-09-14 17:44:14 +0200

    Add dropdown code snippets

--- src/main/kotlin/org/patternfly/showcase/component/Dropdown.kt
+++ src/main/kotlin/org/patternfly/showcase/component/Dropdown.kt
@@ -489,42 +489,281 @@ internal object DropdownCode {
     //language=kotlin
     const val KEBAB: String = """fun main() {
     render {
+        pfDropdown<String> {
+            pfDropdownToggleKebab()
+            pfDropdownItems {
+                pfItem("Action")
+                pfItem("Disabled Action") {
+                    disabled = true
+                }
+                pfSeparator()
+                pfItem("Separated Action")
+            }
+        }
     }
 }"""
 
     //language=kotlin
     const val ICON: String = """fun main() {
     render {
+        pfDropdown<String> {
+            pfDropdownToggleIcon { pfIcon("th".fas()) }
+            pfDropdownItems {
+                pfItem("Action")
+                pfItem("Disabled Action") {
+                    disabled = true
+                }
+                pfSeparator()
+                pfItem("Separated Action")
+            }
+        }
     }
 }"""
 
     //language=kotlin
     const val DESCRIPTION: String = """fun main() {
     render {
+        pfDropdown<String> {
+            pfDropdownToggleText { +"Dropdown" }
+            pfDropdownItems {
+                pfItem("Action 1") {
+                    icon = { pfIcon("cog".fas()) }
+                }
+                pfItem("Action 2") {
+                    description = "This is a description"
+                }
+                pfItem("Action 3") {
+                    icon = { img { src = const("./pf-logo-small.svg") } }
+                    description = "This is a description"
+                }
+                pfSeparator()
+                pfItem("Disabled action 1") {
+                    disabled = true
+                    icon = { pfIcon("cog".fas()) }
+                }
+                pfItem("Disabled action 2") {
+                    disabled = true
+                    description = "This is a description"
+                }
+                pfItem("Disabled action 3") {
+                    disabled = true
+                    icon = { pfIcon("cog".fas()) }
+                    description = "This is a description"
+                }
+            }
+        }
     }
 }"""
 
     //language=kotlin
     const val CHECKBOX_TOGGLE: String = """fun main() {
     render {
+        pfDropdown<String> {
+            pfDropdownToggleCheckbox()
+            pfDropdownItems {
+                pfItem("Action")
+                pfItem("Disabled Action") {
+                    disabled = true
+                }
+                pfSeparator()
+                pfItem("Separated Action")
+            }
+        }
+        pfDropdown<String>(classes = "ml-sm".util()) {
+            pfDropdownToggleCheckbox { +"10 selected" }
+            pfDropdownItems {
+                pfItem("Action")
+                pfItem("Disabled Action") {
+                    disabled = true
+                }
+                pfSeparator()
+                pfItem("Separated Action")
+            }
+        }
     }
 }"""
 
     //language=kotlin
     const val ACTION_TOGGLE: String = """fun main() {
     render {
+        pfDropdown<String> {
+            pfDropdownToggleActionText { +"Action" }
+            pfDropdownItems {
+                pfItem("Action")
+                pfItem("Disabled Action") {
+                    disabled = true
+                }
+                pfSeparator()
+                pfItem("Separated Action")
+            }
+        }
+        pfDropdown<String>(classes = "ml-sm".util()) {
+            pfDropdownToggleActionIcon(icon = pfIcon("cog".fas()))
+            pfDropdownItems {
+                pfItem("Action") {
+                    icon = { pfIcon("cog".fas()) }
+                }
+                pfItem("Disabled Action") {
+                    disabled = true
+                    icon = { pfIcon("bell".fas()) }
+                }
+                pfSeparator()
+                pfItem("Separated Action") {
+                    icon = { pfIcon("cubes".fas()) }
+                }
+            }
+        }
     }
 }"""
 
     //language=kotlin
     const val GROUPS: String = """fun main() {
     render {
+        pfDropdown<String> {
+            pfDropdownToggleText { +"Dropdown" }
+            pfDropdownGroups {
+                pfGroup {
+                    pfItem("Action 1")
+                    pfItem("Action 2")
+                }
+                pfSeparator()
+                pfGroup("Group 1") {
+                    pfItem("Action 1")
+                    pfItem("Action 2")
+                }
+                pfSeparator()
+                pfGroup("Group 2") {
+                    pfItem("Action 1")
+                    pfItem("Action 2")
+                }
+            }
+        }
     }
 }"""
 
     //language=kotlin
     const val REACTIVE: String = """fun main() {
     render {
+        fun currentValue(event: Event) = event.target.unsafeCast<HTMLInputElement>().value
+
+        fun registerEvents(dropdown: Dropdown<String>, name: String) {
+            with(dropdown) {
+                store.clicks
+                    .map { Notification(INFO, "${'$'}name: Clicked on ${'$'}it") }
+                    .handledBy(Notification.store.add)
+                ces.collapsed
+                    .map { Notification(INFO, "${'$'}name: Dropdown collapsed") }
+                    .handledBy(Notification.store.add)
+                ces.expanded
+                    .map { Notification(INFO, "${'$'}name: Dropdown expanded") }
+                    .handledBy(Notification.store.add)
+            }
+        }
+
+        fun items(): List<Entry<String>> = pfItems {
+            pfItem("Action")
+            pfItem("Disabled Action") {
+                disabled = true
+            }
+            pfSeparator()
+            pfItem("Separated Action")
+        }
+
+        fun checkboxItems(): List<Entry<String>> = pfItems {
+            pfItem("Select none")
+            pfItem("Select visible")
+            pfItem("Select all")
+        }
+
+        lateinit var text: Input
+        lateinit var enabled: Switch
+        div(baseClass = classes {
+            +"flex".layout()
+            +"align-items-center".modifier()
+            +"mb-sm".util()
+        }) {
+            text = input(baseClass = classes("form-control".component(), "w-50".util())) {
+                type = const("text")
+                value = const("Dropdown")
+                placeholder = const("Dropdown text")
+            }
+            enabled = pfSwitch("ml-md".util()) {
+                label = const("Enabled")
+                labelOff = const("Disabled")
+                input.checked = const(true)
+            }
+        }
+        pfDropdown<String>(classes = "mt-sm".util()) {
+            pfDropdownToggleText {
+                disabled = enabled.input.changes.states().map { !it }
+                text.keyups.map { currentValue(it) }.bind()
+            }
+            pfDropdownItems()
+            registerEvents(this, "Text")
+            action(items()) handledBy store.update
+        }
+        pfDropdown<String>(classes = "ml-sm".util()) {
+            pfDropdownToggleKebab {
+                disabled = enabled.input.changes.states().map { !it }
+            }
+            pfDropdownItems()
+            registerEvents(this, "Kebab")
+            action(items()) handledBy store.update
+        }
+        pfDropdown<String>(classes = "ml-sm".util()) {
+            pfDropdownToggleIcon {
+                pfIcon("cog".fas())
+                disabled = enabled.input.changes.states().map { !it }
+            }
+            pfDropdownItems()
+            registerEvents(this, "Icon")
+            action(items()) handledBy store.update
+        }
+        br {}
+        pfDropdown<String>(classes = "mt-sm".util()) {
+            pfDropdownToggleCheckbox {
+                disabled = enabled.input.changes.states().map { !it }
+                triState = this@pfDropdown.store.clicks.map {
+                    when (it) {
+                        "Select none" -> TriState.OFF
+                        "Select visible" -> TriState.INDETERMINATE
+                        "Select all" -> TriState.ON
+                        else -> TriState.OFF
+                    }
+                }
+                input.changes.states()
+                    .map { Notification(INFO, "Split button ${'$'}{if (it) "checked" else "unchecked"}") }
+                    .handledBy(Notification.store.add)
+                this@pfDropdown.store.clicks.map { if (it == "Select none") "" else it }.bind()
+            }
+            pfDropdownItems()
+            registerEvents(this, "Split button")
+            action(checkboxItems()) handledBy store.update
+        }
+        br {}
+        pfDropdown<String>(classes = "mt-sm".util()) {
+            pfDropdownToggleActionText {
+                disabled = enabled.input.changes.states().map { !it }
+                text.keyups.map { currentValue(it) }.bind()
+                action.clicks
+                    .map { Notification(INFO, "Action clicked") }
+                    .handledBy(Notification.store.add)
+            }
+            pfDropdownItems()
+            registerEvents(this, "Action text")
+            action(items()) handledBy store.update
+        }
+        pfDropdown<String>(classes = "ml-sm".util()) {
+            pfDropdownToggleActionIcon(pfIcon("cog".fas())) {
+                disabled = enabled.input.changes.states().map { !it }
+                action.clicks
+                    .map { Notification(INFO, "Action clicked") }
+                    .handledBy(Notification.store.add)
+            }
+            pfDropdownItems()
+            registerEvents(this, "Action icon")
+            action(items()) handledBy store.update
+        }
     }
 }"""
 }
