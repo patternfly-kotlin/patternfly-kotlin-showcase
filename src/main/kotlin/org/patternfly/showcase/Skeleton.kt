cc7d8cd3eea021cfa4832ca149afcb2d90c5b9d4
hpehl <harald.pehl@gmail.com>
2021-11-05 16:18:57 +0100

    Adjust to latest patternfly-fritz2 SNAPSHOT

--- src/main/kotlin/org/patternfly/showcase/Skeleton.kt
+++ src/main/kotlin/org/patternfly/showcase/Skeleton.kt
@@ -4,12 +4,9 @@ import dev.fritz2.dom.html.RenderContext
 import dev.fritz2.mvp.PlaceManager
 import dev.fritz2.mvp.managedBy
 import dev.fritz2.mvp.placeRequest
-import org.patternfly.group
-import org.patternfly.groups
-import org.patternfly.item
+import org.patternfly.navigation
 import org.patternfly.page
 import org.patternfly.showcase.component.components
-import org.patternfly.verticalNavigation
 
 fun RenderContext.skeleton(placeManager: PlaceManager) {
     page {
@@ -22,20 +19,16 @@ fun RenderContext.skeleton(placeManager: PlaceManager) {
 //            }
         }
         sidebar {
-            verticalNavigation(placeManager.router, expandable = true) {
-                groups {
-                    group {
-                        item(placeRequest(GET_STARTED), "Get Started")
-                        item(placeRequest(GET_IN_TOUCH), "Get in Touch")
+            navigation(placeManager.router, expandable = true) {
+                item(placeRequest(GET_STARTED), "Get Started")
+                item(placeRequest(GET_IN_TOUCH), "Get in Touch")
+                group("Components") {
+                    components.map {
+                        item(placeRequest(COMPONENT, "id" to it.id), it.name)
                     }
-                    group("Components") {
-                        components.map {
-                            item(placeRequest(COMPONENT, "id" to it.id), it.name)
-                        }
-                    }
-                    group("Demos") {
-                        item(placeRequest(USER_DEMO), "User")
-                    }
+                }
+                group("Demos") {
+                    item(placeRequest(USER_DEMO), "User")
                 }
             }
         }
