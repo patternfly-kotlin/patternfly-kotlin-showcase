a2763ff3802d6cf1e6123ae0c05ff6f75a4aad50
Harald Pehl <harald.pehl@gmail.com>
2022-05-08 17:43:17 +0200

    Slider demo - wip

--- src/main/kotlin/org/patternfly/showcase/component/Slider.kt
+++ src/main/kotlin/org/patternfly/showcase/component/Slider.kt
@@ -2,6 +2,7 @@ package org.patternfly.showcase.component
 
 import dev.fritz2.binding.storeOf
 import dev.fritz2.dom.html.RenderContext
+import kotlinx.coroutines.flow.map
 import org.patternfly.Step
 import org.patternfly.pageSection
 import org.patternfly.slider
@@ -21,59 +22,43 @@ object SliderComponent {
                     storeOf(50),
                     storeOf(25),
                     storeOf(50),
-                    storeOf(50),
                     storeOf(3),
                 )
 
                 h3 { +"Slider value is: "; span { values[index].data.renderText() } }
-                small {
-                    +"(steps: "; b { +"0" }; +", 1, "; b { +"2" }; +", 3, ";b { +"4" };+", 5, ";b { +"6" };+", 7, ";b { +"8" };+")"
-                }
-                slider(
-                    value = values[index],
-                    steps = listOf(
-                        Step(0, "0"),
-                        Step(1),
-                        Step(2, "2"),
-                        Step(3),
-                        Step(4, "4"),
-                        Step(5),
-                        Step(6, "6"),
-                        Step(7),
-                        Step(8, "8"),
-                    )
-                )
-                br { }
-                index++
-
-                h3 { +"Slider value is: "; span { values[index].data.renderText() } }
-                small { +"(min = 0, max = 200, step = 50)" }
-                slider(value = values[index], progression = 0..200 step 50, hideSteps = true)
-                br { }
-                index++
-
-                h3 { +"Slider value is: "; span { values[index].data.renderText() } }
-                small { +"(min = -25, max = 75, step = 10, boundaries not shown)" }
-                slider(value = values[index], progression = -25..75 step 10, hideBoundaries = true, hideSteps = true)
-                br { }
-                index++
-
-                h3 { +"Slider value is: "; span { values[index].data.renderText() } }
-                small { +"(min = -25, max = 75, step = 10, boundaries shown)" }
-                slider(value = values[index], progression = -25..75 step 10, hideSteps = true)
-                br { }
-                index++
-
-                h3 { +"Slider value is: "; span { values[index].data.renderText() } }
-                small { +"(min = -25, max = 75, step = 10, boundaries shown, ticks not shown)" }
-                slider(progression = -25..75 step 10, hideSteps = true, hideTicks = true) {
-                    value(values[index])
+                small { +"(min = 0, max = 8, show boundaries, show event steps, show ticks)" }
+                slider(values[index], Step(0, "0")..Step(8, "8")) {
+                    steps { step, _, _, _ -> step % 2 == 0 }
+                    showTicks()
                 }
                 br { }
                 index++
 
                 h3 { +"Slider value is: "; span { values[index].data.renderText() } }
-                small { +"(steps: A, B, C, D, E, F)" }
+                small { +"(min = 0, max = 200, step = 50, show boundaries, hide steps, show ticks)" }
+                slider(value = values[index], steps = 0..200 step 50) {
+                    showTicks()
+                }
+                br { }
+                index++
+
+                h3 { +"Slider value is: "; span { values[index].data.renderText() } }
+                small { +"(min = -25, max = 75, step = 10, hide boundaries, hide steps, show ticks)" }
+                slider(value = values[index], steps = -25..75 step 10) {
+                    hideBoundaries()
+                    showTicks()
+                }
+                br { }
+                index++
+
+                h3 { +"Slider value is: "; span { values[index].data.renderText() } }
+                small { +"(min = -25, max = 75, step = 10, show boundaries, hide steps, hide ticks)" }
+                slider(value = values[index], steps = -25..75 step 10)
+                br { }
+                index++
+
+                h3 { +"Slider value is: "; span { values[index].data.renderText() } }
+                small { +"(custom steps: A, B, C, D, E, F, show boundaries, show steps, show ticks)" }
                 slider(
                     value = values[index],
                     steps = listOf(
@@ -85,6 +70,21 @@ object SliderComponent {
                         Step(5, "F"),
                     )
                 )
+            }
+            snippet("Continuous", SliderCode.CONTINUOUS) {
+                var index = 0
+                val values = arrayOf(
+                    storeOf(77),
+                    storeOf(23),
+                )
+
+                h3 { +"Slider value is: "; span { values[index].data.renderText() } }
+                slider(value = values[index], steps = 0..100)
+                br { }
+                index++
+
+                h3 { +"Slider value is: "; span { values[index].data.map { "$it%" }.renderText() } }
+                slider(value = values[index], steps = Step(0, "0%")..Step(100, "100%"))
             }
         }
     }
